e9d4185ad9845128701f19abff41464bed80aa08
dana tinnerello <d.tinnerello@alumno.um.edu.ar>
2025-08-22 13:06:52 -0300

    Estructura basica de la clase juego y dados

--- core/dice.py
+++ core/dice.py
@@ -4,3 +4,31 @@
 -Administrar tiradas dobles
 
 '''
+
+import random
+
+
+
+class Dados:
+    def __init__(self):
+        self.__dado1 = 0
+        self.__dado2 = 0
+        self.__tiradas_restantes = []
+
+    def tirar_dados(self):
+        self.__dado1 = random.randint(1, 6)
+        self.__dado2 = random.randint(1, 6)
+        if self.__dado1 == self.__dado2:
+            self.__tiradas_restantes = [self.__dado1] * 4   #duplicamos si los dados son iguales
+        else:
+            self.__tiradas_restantes = [self.__dado1, self.__dado2] #sino solo guarda los valores
+        return self.__dado1, self.__dado2
+
+    def obtener_tiradas_restantes(self):
+        return self.__tiradas_restantes
+
+    def usar_tirada(self, valor): #controla si el valor esta en las tiradas restantes
+        if valor in self.__tiradas_restantes:
+            self.__tiradas_restantes.remove(valor)
+            return True
+        return False
