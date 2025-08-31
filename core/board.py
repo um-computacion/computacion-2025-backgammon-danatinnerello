ca28176a3c35f22517315169250f069680a335a9
dana tinnerello <d.tinnerello@alumno.um.edu.ar>
2025-08-31 15:52:07 -0300

    Avance en el desarrollo de clases y funciones mas especificas

--- core/board.py
+++ core/board.py
@@ -5,10 +5,12 @@
 -guardar movimiento
 
 '''
+
+
 class Tablero:
     def __init__(self):
         self.__contenedor__ = [[] for _ in range(24)] 
-        #Al iniciarse 
+        #Al iniciarse, creamos el tablero con la posicion inicial de las fichas
         self.__contenedor__[0]=["Negra"]*2
         self.__contenedor__[11]=["Negra"]*5
         self.__contenedor__[16]=["Negra"]*3
@@ -19,8 +21,18 @@ class Tablero:
         self.__contenedor__[7]=["Blanca"]*3
         self.__contenedor__[5]=["Blanca"]*5
 
+        self.__barra__ = {"Blanca": [], "Negra": []} #creamos un diccionario para la barra
+        self.__afuera__ = {"Blanca": [], "Negra": []} #creamos un diccionario para las fichas afuera del tablero
+
+
     def mostrar_contenedor(self):
         return self.__contenedor__
+    
+    def mostrar_barra(self):
+        return self.__barra__
+
+    def mostrar_afuera(self):
+        return self.__afuera__
     
     def mover_ficha(self,ficha,desde,hacia):
         #verifica que la posiicon este entre 0 y 23
@@ -34,5 +46,38 @@ class Tablero:
         #guarda ficha
         self.__contenedor__[hacia].append(ficha)
 
+    def validar_movimiento(self, color, hacia):
+        #valida el movimiento
+        if hacia < 0 or hacia > 23: #primero si esta en el rango
+            return False
+        destino = self.__contenedor__[hacia]
+        if len(destino)>= 2 and destino[0]!= color: #luego si hay 2 o mas fichas enemigas
+            return False
+        return True
+
+    def enviar_a_barra(self, desde):
+        #Si cae en una posicion con 1 ficha enemiga o del otro jugador, la manda a la barra
+        if not self.__contenedor__[desde]:
+            return
+        ficha = self.__contenedor__[desde].pop() #elimina del contenedor
+        self.__barra__[ficha].append(ficha) #la agrega a la barra
+
+    def mover_desde_barra(self, color, hacia):
+        #Saca ficha de la barra(si el movimiento es válido) y vuelve a estar en juego
+        if not self.validar_movimiento(color, hacia):
+            return False
+        if self.__barra__[color]:
+            ficha = self.__barra__[color].pop() #saca la ficha de la barra
+            self.__contenedor__[hacia].append(ficha) #la agrega al contenedor
+            return True
+        return False
+
+    def sacar_ficha(self, color, desde):
+        #Cuando todas las fichas están en el ultimo cuadrante del contendor, se pueden sacar
+        if self.__contenedor__[desde] and self.__contenedor__[desde][-1] == color:
+            ficha = self.__contenedor__[desde].pop() #saca la ficha del contenedor
+            self.__afuera__[color].append(ficha) #agregala ficha a fuera
+            return True
+        return False
   
     
--- core/player.py
+++ core/player.py
@@ -13,7 +13,10 @@ class Jugador:
         self.__fichas__= 15
         self.__fichas_restantes__= 15
 
-
+    
+    def mostrar_fichas_restantes(self):
+        return self.__fichas_restantes__
+    
     def obtener_nombre(self):
         return self.__nombre__
 
@@ -23,8 +26,20 @@ class Jugador:
     def gano(self): #gana cuando ya no tiene mas fichas
         return self.__fichas_restantes__ == 0
 
-    def sacar_ficha(self): #metodo para ir restando las fichass que va sacando del tablero
-        if self.__fichas_restantes__ > 0:
-            self.__fichas_restantes__ -= 1
+    def sacar_ficha_a_afuera(self): #metodo para ir restando las fichass que va sacando del tablero
+        if self.__fichas_restantes__ > 0: # si es mayor que cero
+            self.__fichas_restantes__ -= 1 #le resta una ficha
             return True
         return False
+    
+    def agregar_a_barra(self):
+        self.__barra__ += 1 #agrega una ficha a la barra
+
+    def quitar_de_barra(self):
+        if self.__barra__ > 0: #si hay mas de una ficha en la barra
+            self.__barra__ -= 1 # le resta una
+            return True
+        return False
+
+    def tiene_en_barra(self): #muestra si tiene fichas en la barra
+        return self.__barra__ > 0
