ab24f3b730993c12e2eadd63fbc7443e1ddac856
dana tinnerello <d.tinnerello@alumno.um.edu.ar>
2025-10-03 08:29:01 -0300

    Inicializacion del pygame y fondo

--- pygame_ui/main.py
+++ pygame_ui/main.py
@@ -7,3 +7,35 @@ Responsabilidad:
 - Captura eventos (mouse, teclado) usando events.py.
 - Mantiene el loop de juego (update → draw → events).
 """
+
+import pygame
+
+
+def main():
+    # Inicializar pygame
+    pygame.init()
+    pantalla = pygame.display.set_mode((1000, 600))
+    pygame.display.set_caption("Backgammon - Pygame")
+
+    reloj = pygame.time.Clock()
+  
+    corriendo = True
+    while corriendo:
+        # Manejo de eventos
+        for evento in pygame.event.get():
+            if evento.type == pygame.QUIT:
+                corriendo = False
+
+        # Fondo color madera
+        pantalla.fill((220, 190, 160))
+
+        # Dibujar tablero
+      
+        # Actualizar pantalla
+        pygame.display.flip()
+        reloj.tick(60)
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
